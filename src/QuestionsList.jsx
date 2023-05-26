9225117105757e6be21a33120789a5faa537ba53
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-05-26 20:12:32 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Das Produkt ist in 10 Tagen lieferbar?",
+        answersList: [
+            {answer:"Es kann innerhalb von 10 Tagen geliefert werden.", isCorrect:true},
+            {answer:"Es ist in 10 Tagen geliefert worden.", isCorrect:false},
+            {answer:"Es muss in 10 Tagen geliefert werden.", isCorrect:false},
+        ]
+    },{
+        question:"Der Zucker löst sich im Kaffee auf.",
+        answersList: [
+            {answer:"Er ist leicht lösbar.", isCorrect:false},
+            {answer:"Er ist in Flüssigkeit löslich.", isCorrect:true},
+            {answer:"Er ist die einfachste Lösung.", isCorrect:false},
+        ]
+    },{
+        question:"Ich kann nicht die ganze Wohnung selber streichen.",
+        answersList: [
+            {answer:"Ich hätte sie gerne streichen lassen.", isCorrect:false},
+            {answer:"Wir werden unsere auch nicht streichen lassen.", isCorrect:false},
+            {answer:"Du kannst sie von einem Maler streichen lassen.", isCorrect:true},
+        ]
+    },{
+        question:"Der Antrag ist innerhalb von 20 Tagen zu stellen.",
+        answersList: [
+            {answer:"Er muss innerhalb dieser Frist gestellt werden.", isCorrect:true},
+            {answer:"Er wird in 20 Tagen präsentiert.", isCorrect:false},
+            {answer:"Er kann auch noch später vorgelegt werden.", isCorrect:false},
+        ]
+    },{
+        question:"Die Reparatur lässt sich nicht für weniger als 100 Euro machen.",
+        answersList: [
+            {answer:"Sie ist auch billiger zu haben.", isCorrect:false},
+            {answer:"Sie kann nicht billiger gemacht werden.", isCorrect:true},
+            {answer:"Sie muss für weniger Geld gemacht werden.", isCorrect:false},
+        ]
+    },{
+        question:"Mit den Vorbereitungen wird schon im Herbst begonnen.",
+        answersList: [
+            {answer:"Man begann schon im Herbst damit.", isCorrect:false},
+            {answer:"Man sollte im Herbst damit anfangen.", isCorrect:false},
+            {answer:"Man beginnt schon im Herbst mit ihnen.", isCorrect:true},
+        ]
+    },{
+        question:"Am Wochenende muss bis Mitternacht gearbeitet werden.",
+        answersList: [
+            {answer:"Wir müssen da bis 24 Uhr arbeiten.", isCorrect:true},
+            {answer:"Wir konnten bis spät in die Nacht arbeiten.", isCorrect:false},
+            {answer:"Wir sollen bis zum nächsten Morgen arbeiten.", isCorrect:false},
+        ]
+    },{
+        question:"Eine unverständliche Reaktion ist eine,",
+        answersList: [
+            {answer:"die den meisten Leuten klar ist.", isCorrect:false},
+            {answer:"die keiner richtig versteht.", isCorrect:true},
+            {answer:"die niemand verstehen will.", isCorrect:false},
+        ]
+    },{
+        question:"Hast du irgendetwas gegen Kopfschmerzen dabei?",
+        answersList: [
+            {answer:"Nein, ich bin aus gutem Grund dagegen.", isCorrect:false},
+            {answer:"Eine Tablette wäre von Hilfe gewesen.", isCorrect:false},
+            {answer:"Ja, nimm eine von diesen Tabletten.", isCorrect:true},
+        ]
+    },{
+        question:"Hat irgendjemand den Unfall gesehen?",
+        answersList: [
+            {answer:"Ja, jemand in dem Restaurant hat es beobachtet.", isCorrect:true},
+            {answer:"Nein, es ist nichts zu sehen in dem Restaurant.", isCorrect:false},
+            {answer:"Einer aus dem Restaurant wollte es sehen.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Sie müssen sich wohl in der U-Bahn angesteckt haben.",
+        answersList: [
+            {answer:"Das kann ich mir auch vorstellen.", isCorrect:true},
+            {answer:"Das nächste Mal kann ich mich nicht anstecken.", isCorrect:false},
+            {answer:"Wie kann ich eine Ansteckung verhindern?", isCorrect:false},
+        ]
+    },{
+        question:"Indem man schädliche Verhaltensweisen vermeidet,",
+        answersList: [
+            {answer:"kann man schneller wieder krank werden.", isCorrect:false},
+            {answer:"kann man sich einer besseren Gesundheit erfreuen.", isCorrect:true},
+            {answer:"kann man alle Schmerzen verhindern.", isCorrect:false},
+        ]
+    },{
+        question:"Die Vorteile der Akupunktur werden stark infrage gestellt.",
+        answersList: [
+            {answer:"Obwohl alternative Methoden nicht zur Debatte stehen.", isCorrect:false},
+            {answer:"Trotzdem sollten wir nicht ihre Wirkungen kritisieren.", isCorrect:false},
+            {answer:"Obwohl sie seit Jahrhunderten angewendet wird.", isCorrect:true},
+        ]
+    },{
+        question:"Können Sie noch weitere Argumente in die Diskussion einbringen?",
+        answersList: [
+            {answer:"Ich möchte noch auf zwei Aspekte hinweisen.", isCorrect:true},
+            {answer:"Ich konnte noch einen weiteren Aspekt hinzufügen.", isCorrect:false},
+            {answer:"Ich wäre noch auf andere Aspekte eingegangen.", isCorrect:false},
+        ]
+    },{
+        question:"Die Therapie verspricht eine schnelle Heilung,",
+        answersList: [
+            {answer:"so dass es die Wissenschaft nicht begründen kann.", isCorrect:false},
+            {answer:"ohne dies wissenschaftlich begründen zu können.", isCorrect:true},
+            {answer:"ohne dass ein Erfolg garantiert werden kann.", isCorrect:false},
+        ]
+    },{
+        question:"Man könnte sein unvernünftiges Verhalten dadurch beeinflussen,",
+        answersList: [
+            {answer:"indem man sich neutral verhält.", isCorrect:false},
+            {answer:"dass man es ihm vernünftig erklärt.", isCorrect:false},
+            {answer:"dass man ihm die Konsequenzen aufzeigt.", isCorrect:true},
+        ]
+    },{
+        question:"Durch häufiges Wiederholen",
+        answersList: [
+            {answer:"kann es zu einer neuen Gewohnheit werden.", isCorrect:true},
+            {answer:"kann man sich nicht schneller daran gewöhnen.", isCorrect:false},
+            {answer:"gewöhnen Sie sich häufig schnell daran.", isCorrect:false},
+        ]
+    },{
+        question:"Wir sollten die Symptome behandeln,",
+        answersList: [
+            {answer:"ohne dass wir den Menschen dabei berücksichtigen.", isCorrect:false},
+            {answer:"ohne den Menschen dabei zu vergessen.", isCorrect:true},
+            {answer:"anstatt den ganzen Menschen zu vergessen.", isCorrect:false},
+        ]
+    },{
+        question:"Bevor Sie eine Reise ins Ausland unternehmen",
+        answersList: [
+            {answer:"muss die Versicherung alles überprüfen.", isCorrect:false},
+            {answer:"kann der Versicherte alles abschließen.", isCorrect:false},
+            {answer:"sollten sie eine Versicherung abschließen.", isCorrect:true},
+        ]
+    },{
+        question:"Anke ist Krankenschwester geworden",
+        answersList: [
+            {answer:"obwohl sie lange Arbeitszeiten hat.", isCorrect:true},
+            {answer:"weil sie lange arbeiten möchte.", isCorrect:false},
+            {answer:"denn lange Arbeitszeiten sind wichtig.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Nach der Vorstellung richtete sich der Regisseur",
+        answersList: [
+            {answer:"an das protestierende Publikum.", isCorrect:true},
+            {answer:"an das Publikum, das aus Protest fernbleibt.", isCorrect:false},
+            {answer:"an das enttäuschende Publikum.", isCorrect:false},
+        ]
+    },{
+        question:"Die ständig stechenden Schmerzen",
+        answersList: [
+            {answer:"müssen nicht so schnell behandelt werden.", isCorrect:false},
+            {answer:"sollten sie unbedingt untersuchen lassen.", isCorrect:true},
+            {answer:"können sie zuhause selbst behandeln.", isCorrect:false},
+        ]
+    },{
+        question:"Dialektsprecher werden leider oft diskrimimiert,",
+        answersList: [
+            {answer:"während sie nur Hochdeutsch sprechen.", isCorrect:false},
+            {answer:"im Vergleich zu den Leuten, die kein Hochdeutsch sprechen.", isCorrect:false},
+            {answer:"im Gegensatz zu Leuten, die nur Hochdeutsch sprechen.", isCorrect:true},
+        ]
+    },{
+        question:"Im Gegensatz zum Leben auf dem Land",
+        answersList: [
+            {answer:"lässt es sich in der Stadt auch ganz gut leben.", isCorrect:true},
+            {answer:"lassen sie uns in der Stadt leben.", isCorrect:false},
+            {answer:"ließ er uns auch in die Stadt fahren.", isCorrect:false},
+        ]
+    },{
+        question:"Die ständig reklamierenden Gäste",
+        answersList: [
+            {answer:"werden von den Angestellten herausgefordert.", isCorrect:false},
+            {answer:"waren eine Forderung der Angestellten.", isCorrect:false},
+            {answer:"sind für die Angestellten eine Herausforderung.", isCorrect:true},
+        ]
+    },{
+        question:"Schon vor Jahren nicht bezahlte Rechnungen",
+        answersList: [
+            {answer:"können sie nicht mehr reklamieren.", isCorrect:true},
+            {answer:"sollten sie auf keinen Fall aufbewahren.", isCorrect:false},
+            {answer:"kann man nicht so einfach begleichen.", isCorrect:false},
+        ]
+    },{
+        question:"Aufgrund einer Protestaktion",
+        answersList: [
+            {answer:"werden die Verkehrsprobleme blockiert.", isCorrect:false},
+            {answer:"war gestern die Autobahn stundenlang blockiert.", isCorrect:true},
+            {answer:"blockieren die Verkehrsteilnehmer ihre Fahrzeuge.", isCorrect:false},
+        ]
+    },{
+        question:"Problematisch könnte es werden",
+        answersList: [
+            {answer:"indem sie selbst behandelt werden.", isCorrect:false},
+            {answer:"bei einer Behandlung ohne Schmerzen.", isCorrect:false},
+            {answer:"wenn sie sich selbst behandeln.", isCorrect:true},
+        ]
+    },{
+        question:"Das Experiment war für ihn",
+        answersList: [
+            {answer:"eine große Herausforderung.", isCorrect:true},
+            {answer:"die Forderung heraus zu kommen.", isCorrect:false},
+            {answer:"eine gute Entspannungsübung.", isCorrect:false},
+        ]
+    },{
+        question:"Mit diesen Übungen können Sie sich gut",
+        answersList: [
+            {answer:"auf die Tests konzentrieren.", isCorrect:false},
+            {answer:"vorbereiten, indem Sie nichts wiederholen.", isCorrect:false},
+            {answer:"auf die kommenden Prüfungen vorbereiten.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
